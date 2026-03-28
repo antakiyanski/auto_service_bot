f1fc250c428e97f3344b11ec05b2816f68573ec2
sagindikovabdulaziz <abdulazizsagindikov@gmail.com>
2026-03-28 09:56:34 +0500

    abdulaziz

---
+++ keyboards.py
@@ -0,0 +1,11 @@
+from aiogram.types
+import InlineKeyboardMarkup,inlineKeyboardButton
+from config import SERVICES
+
+def get_services_kb():
+    buttons =[]
+    for key,(name,price) in SERVICES.items():
+        buttons.append([
+            InlineKeyboardButton(text=f"{name}-{price}",callback_data=f"ser_{key}")
+        ])
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
